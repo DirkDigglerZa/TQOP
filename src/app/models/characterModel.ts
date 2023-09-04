15dcf948e304d83527757497575c83b47906d014
Bosch <BOSCHQX@legal.regn.net>
2023-09-04 16:40:52 +0200

    added the models

---
+++ src/app/models/characterModel.ts
@@ -0,0 +1,32 @@
+export interface CharacterModel {
+    headerVersion: string;
+    playerCharacterClass: string;
+    playerClassTag: string;
+    playerLevel: string;
+    isInMainQuest: string;
+    playerVersion: string;
+    myPlayerName: string;
+    numTutorialPagesV2: string;
+    primarySkill5: string;
+    secondarySkill5skillActive: string;
+    currentStatsCharLevel: string;
+    currentStatsExperiencePoints: string;
+    modifierPoints: string;
+    skillPoints: string;
+    playTimeInSeconds: string;
+    numberOfDeaths: string;
+    numberOfKills: string;
+    experienceFromKills: string;
+    healthPotionsUsed: string;
+    manaPotionsUsed: string;
+    maxLevel: string;
+    numHitsReceived: string;
+    numHitsInflicted: string;
+    greatestDamageInflicted: string;
+    greatestMonsterKilledName: string;
+    greatestMonsterKilledLevel: string;
+    greatestMonsterKilledLifeAndMana: string;
+    criticalHitsInflicted: string;
+    criticalHitsReceived: string;
+}
+
